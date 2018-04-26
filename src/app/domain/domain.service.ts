9f8ec3fd9c05fbb8f75e69f3cd8e5a024f9e7407
Samuel Waskow <samuel@hicaesar.com>
2018-04-26 13:49:48 -0300

    #4

--- src/app/domain/domain.service.ts
+++ src/app/domain/domain.service.ts
@@ -23,7 +23,7 @@ export class DomainService {
      */
     get(callback: Function) {
 
-        return this.http.get<Array<Domain>>(`${environment.apihost}/domain`)
+        return this.http.get<Array<Domain>>(`${environment.apihost}/domain/list`)
             .subscribe(data => {
                 const out = new Response(true);
                 out.list = data;
