07f9d890524d751567ea3109292024cd857b016b
Samuel Waskow <samuel@hicaesar.com>
2018-04-26 09:05:30 -0300

    #4

--- src/app/domain/domain.component.spec.ts
+++ src/app/domain/domain.component.spec.ts
@@ -1,7 +1,10 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { DomainComponent } from './domain.component';
 
 describe('DomainComponent', () => {
+
+    let fixture: ComponentFixture<DomainComponent>;
+    let component: DomainComponent;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
@@ -9,23 +12,24 @@ describe('DomainComponent', () => {
                 DomainComponent
             ],
         }).compileComponents();
+
+        fixture = TestBed.createComponent(DomainComponent);
+        component = fixture.debugElement.componentInstance;
     }));
 
-    it('should create the domain', async(() => {
-        const fixture = TestBed.createComponent(DomainComponent);
-        const domain = fixture.debugElement.componentInstance;
-        expect(domain).toBeTruthy();
+    it('should create the component', async(() => {
+        expect(component).toBeTruthy();
     }));
 
     it('should render title in a h2 tag', async(() => {
-        const fixture = TestBed.createComponent(DomainComponent);
+
         fixture.detectChanges();
         const compiled = fixture.debugElement.nativeElement;
         expect(compiled.querySelector('h2').textContent).toContain('Domains');
     }));
 
     it('should render subtitle in a h6 tag', async(() => {
-        const fixture = TestBed.createComponent(DomainComponent);
+
         fixture.detectChanges();
         const compiled = fixture.debugElement.nativeElement;
         expect(compiled.querySelector('h6').textContent).toContain('Manage knowledge domains');
